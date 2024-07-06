c756ecd6bf4f7b954c64e30bbacbdb7e056f625c
ankittt9 <ankitptl7024@gmail.com>
2024-07-06 16:52:45 +0530

    outside template create folder

---
+++ project/app/urls.py
@@ -0,0 +1,13 @@
+
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    path('home/',home,name='home'),
+    path('shop/',shop,name='shop'),
+    path('about/',about,name='about'),
+    path('blog/',blog,name='blog'),
+    path('contact/',contact,name='contact')
+
+   
+]
